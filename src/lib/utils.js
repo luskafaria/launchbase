9ddaab80e39a790ca477fe8654b99fd6d2511a78
luskafaria <lucashpharia@gmail.com>
2019-12-30 11:43:39 -0300

    first commit

---
+++ src/lib/utils.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+  date(timestamp) {
+    const date = new Date(timestamp)
+    //yyyy
+    const year = date.getUTCFullYear();
+    //mm
+    const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+    //dd
+    const day = `0${date.getUTCDate()}`.slice(-2);
+    //yyyy/mm/dd
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+
+    return {
+      minutes,
+      hour,
+      day,
+      month,
+      year,
+      iso: `${year}-${month}-${day}`,
+      birthDay: `${day}/${month}`,
+      format: `${day}/${month}/${year}`
+    };
+
+  },
+
+  formatPrice(price) {
+
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price / 100)
+  }
+}
